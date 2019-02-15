d0263ab79e3a53eee2e6ed1665a01cb7c5fcb431
Mikhail Istomin <mikhail_istomin@epam.com>
2019-02-15 10:17:22 +0300

    move objects data to objects.js file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,5 @@
 import * as THREE from "three";
-
-const boxes = [
-  {
-    w: 5,
-    h: 20,
-    d: 5,
-    x: 2.5,
-    y: 10,
-    z: 2.5,
-    boxColor: 0x003300,
-    cageColor: 0xffffff
-  },
-  {
-    w: 5,
-    h: 24,
-    d: 5,
-    x: 7.5,
-    y: 12,
-    z: 2.5,
-    boxColor: 0x003377,
-    cageColor: 0xffffff
-  },
-  {
-    w: 5,
-    h: 18,
-    d: 5,
-    x: 2.5,
-    y: 9,
-    z: 7.5,
-    boxColor: 0x663377,
-    cageColor: 0xffffff
-  }
-];
+import { boxes } from "./objects.js";
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
